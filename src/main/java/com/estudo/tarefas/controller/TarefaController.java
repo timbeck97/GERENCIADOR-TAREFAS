970880b510dfa52c5c6cb17ef501799c0b169067
Tim Beck <timbeck1997@hotmail.com>
2022-02-02 19:23:26 -0300

    Criado metodo para deletar tarefa

--- src/main/java/com/estudo/tarefas/controller/TarefaController.java
+++ src/main/java/com/estudo/tarefas/controller/TarefaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -33,4 +34,10 @@ public class TarefaController {
 	public String buscarTarefas() {
 		return "/tarefas/buscartarefas";
 	}
+	@GetMapping(value = "/tarefas/{id}")
+	public String deletaTarefa(@PathVariable("id")Long id) {
+		rep.deleteById(id);
+		return "redirect:/listatarefas";
+	}
+	
 }
