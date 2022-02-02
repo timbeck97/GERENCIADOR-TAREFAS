7b804c5a0e5f59c56c7b8d7af6383022d5eee8a4
Tim Beck <timbeck1997@hotmail.com>
2022-02-01 22:09:17 -0300

    Criado metodo para listar tarefas e metodo para endpoint usado para buscar tarefa especifica

--- src/main/java/com/estudo/tarefas/controller/TarefaController.java
+++ src/main/java/com/estudo/tarefas/controller/TarefaController.java
@@ -1,19 +1,36 @@
 package com.estudo.tarefas.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.estudo.tarefas.model.Tarefa;
+import com.estudo.tarefas.repositories.TarefaRepositorie;
 
 @Controller
 @RequestMapping("/")
 public class TarefaController {
+	@Autowired
+	private TarefaRepositorie rep;
 	
 	@GetMapping
 	public String index() {
 		return "index";
 	}
 	@GetMapping(value = "/listatarefas")
-	public String listaTarefas() {
-		return "/tarefas/listatarefas";
+	public ModelAndView listaTarefas() {
+		ModelAndView mv=new ModelAndView("tarefas/listatarefas");
+		List<Tarefa> tarefas=rep.findAll();
+		mv.addObject("tarefas", tarefas);
+		return mv;
+	}
+	
+	@GetMapping(value = "/buscartarefas")
+	public String buscarTarefas() {
+		return "/tarefas/buscartarefas";
 	}
 }
